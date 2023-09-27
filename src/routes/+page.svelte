64742e00538a4d48e5d4a29c28a9fdbe9083938c
Simon Walker <simon@simonwalker.tech>
2023-09-26 21:56:15 -0600

    basic webgl spinny cube

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
+<script lang="ts">
+    import Screen from "./screen/Screen.svelte";
+</script>
+
+<h1 class="text-xl font-bold w-full text-center p-2">
+    Crossy Road
 </h1>
 
+<!-- WebGL Canvas -->
+<div class="outline outline-2 text-white">
+    <Screen/>
+</div>
+
 <style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
 </style>
---
+++ src/routes/screen/Screen.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import {main} from "$lib/webGL/init";
+    import {onMount} from "svelte";
+
+    let canvas;
+    let clientWidth, clientHeight;
+    let gl;
+
+    function canvasResize() {
+        clientWidth = canvas?.clientWidth;
+        clientHeight = canvas?.clientHeight;
+        gl?.viewport(0, 0, clientWidth, clientHeight)
+    }
+
+    onMount(() => {
+        gl = main(canvas);
+        canvasResize();
+    });
+</script>
+
+<svelte:window on:resize={canvasResize}/>
+
+<!-- WebGL Canvas -->
+<canvas id="screen"
+        class="w-full h-full"
+        bind:this={canvas}
+        bind:clientWidth
+        bind:clientHeight
+        width={clientWidth}
+        height={clientHeight}
+    >
+</canvas>
